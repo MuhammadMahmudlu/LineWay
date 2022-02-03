8a048117535bb6c34cf53de366fb327728343928
Muhammad <muhammadmahmudlu@gmail.com>
2022-02-04 00:45:34 +0400

    Js lesson

---
+++ JS.lesson/Lesson1/script.js
@@ -0,0 +1,34 @@
+// one line comment
+
+/*
+
+more line comment
+*/
+
+//Ekrana mətni çap etmək ucun istifade olunan təqlər
+
+// document.write("Hi JS")
+// document.writeln("Hi js")
+
+
+
+// console.log("Hi Js")
+
+// alert( "Salam Js");
+
+
+
+ document.write("Hi JS" + "<br>")
+debugger
+document.write("Hi JS" + "<br>")
+debugger
+
+document.write("Hi JS" + "<br>")
+debugger
+
+document.write("Hi JS" + "<br>")
+debugger
+
+document.write("Hi JS" + "<br>")
+debugger
+
---
+++ JS.lesson/lesson2/main.js
@@ -0,0 +1,13 @@
+//Primitive (Bəsit) Data type -start
+
+ var infotext ="Salam1 ";     //String (mətn) data type ('hi js')
+  
+  var infoText2 ='Salam2';  //String (mətn) data type ("hi js" + "<br>" + "eleykum Salam")
+   var infoNumber = 10.3*8/4;   //Number (reqem) data type (5)
+   var infoBoolean = false;  // Boolean (mentiqi) data type (true/false)
+
+document.writeln(infotext + "<br>" + infoText2 + "<br>" + infoNumber + "<br>" + infoBoolean + "<br>")
+
+
+
+//composite (birleşik) data type
